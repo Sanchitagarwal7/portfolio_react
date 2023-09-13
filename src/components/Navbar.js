187eebbe867bfd0f5d98ffdc96c4ce36700035df
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-13 17:00:56 +0530

    trying to make navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="navigation">
+        <ul>
+            <li className="list">
+                <a href="?">
+                    <span className="icon">
+                    <FontAwesomeIcon icon="fa-solid fa-house" />
+                    </span>
+                    <span className="text">Home</span>
+                </a>
+            </li>
+            <li className="list">
+                <a href="?">About Me</a>
+            </li>
+            <li className="list">
+                <a href="?">Projects</a>
+            </li>
+            <li className="list">
+                <a href="?">Skills</a>
+            </li>
+            <li className="list">
+                <a href="?">Contact Me</a>
+            </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
