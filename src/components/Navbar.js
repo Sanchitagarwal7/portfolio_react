f4326fe5ad55d2c68ea28721de6040d8d76f1114
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-26 16:29:38 +0530

    navbar frontend completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,80 +3,38 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="?">
-            Navbar
+<nav className="navbar navbar-expand-lg">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="?">Home</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100">
+        <li className="nav-item">
+          <a className="nav-link" href="?">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="?">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="?">Skills</a>
+        </li>
+        <li className="nav-item dropdown ml-auto">
+          <a className="nav-link dropdown-toggle" href="?" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Socials
           </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  About
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  Projects
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  Skills
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  Projects
-                </a>
-              </li>
-              <div class="nav-item dropdown">
-                <button
-                  class="btn dropdown-toggle"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Connect with Me!
-                </button>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="#linkedin">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#mail">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#github">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </ul>
-          </div>
-        </div>
-      </nav>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="?">LinkedIn</a></li>
+            <li><a className="dropdown-item" href="?">Github</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="?">Email</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </>
   );
 };
