ebd756bc02f06ca92b2bc7884863211590a131a4
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-26 23:20:37 +0530

    made home banner without neon button and animation

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import image from '../IMG_20230807_012007_120.jpg';
+import ReactTyped from 'react-typed';
+
+const Home = () => {
+  return (
+    <div>
+    <section id='banner' className='my-5'>
+    <Container>
+        <Row>
+            <Col xs={12} md={6} xl={7}>
+            <h6 id='obi'>Hello There!</h6>
+            <h1 id='name'> I am Sanchit Agarwal</h1>
+            <h4>$ <span><ReactTyped strings={["Web Developer", "Programmer"]} typeSpeed={100} loop backSpeed={50} cursorChar='_' showCursor={true}/></span></h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facilis accusamus nobis saepe nam fugiat optio ut. Placeat deleniti, quia laborum mollitia quod nulla? Tenetur necessitatibus, placeat voluptatem veniam voluptate, praesentium libero voluptatibus veritatis modi fuga, consequuntur totam fugit dolorum facilis perspiciatis tempore nam autem obcaecati ratione neque enim? Qui?</p>
+            <a id='LinkedIn' className='mx-2' href='/'><i class="fa-brands fa-linkedin fa-xl"></i></a>
+            <a id='Github' className='mx-2' href='/'><i class="fa-brands fa-github fa-xl"></i></a>
+            <a id='email' className='mx-2' href='/'><i class="fa-regular fa-envelope fa-xl"></i></a>
+            <a id='Instagram' className='mx-2' href='/'><i class="fa-brands fa-instagram fa-xl"></i></a>
+            </Col>
+            <Col xs={12} md={6} xl={5} style={{textAlign: "center"}}>
+            <img className="floating" src={image} alt="my_picture" />
+            </Col>
+        </Row>
+    </Container>
+    </section>
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,27 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Navbar = () => {
+const Navbarr = () => {
   return (
     <>
-<nav className="navbar navbar-expand-lg">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="?">Home</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100">
-        <li className="nav-item">
-          <a className="nav-link" href="?">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="?">Projects</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="?">Skills</a>
-        </li>
-        <li className="nav-item dropdown ml-auto">
-          <a className="nav-link dropdown-toggle" href="?" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Socials
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="?">LinkedIn</a></li>
-            <li><a className="dropdown-item" href="?">Github</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="?">Email</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <Navbar expand="md" className="navbar">
+      <Container>
+        <Navbar.Brand href="#home" className="navbar-brand">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <Nav.Link href="/" className="nav-link">About</Nav.Link>
+            <Nav.Link href="/" className="nav-link">Projects</Nav.Link>
+            <Nav.Link href="/" className="nav-link">Skills</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   );
 };
 
-export default Navbar;
+export default Navbarr;
