45d402c990fb5c89360fa0eb648bd7641c3eb088
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-27 18:53:04 +0530

    Banner Frontend done without animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,12 +14,13 @@ const Home = () => {
             <h1 id='name'> I am Sanchit Agarwal</h1>
             <h4>$ <span><ReactTyped strings={["Web Developer", "Programmer"]} typeSpeed={100} loop backSpeed={50} cursorChar='_' showCursor={true}/></span></h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facilis accusamus nobis saepe nam fugiat optio ut. Placeat deleniti, quia laborum mollitia quod nulla? Tenetur necessitatibus, placeat voluptatem veniam voluptate, praesentium libero voluptatibus veritatis modi fuga, consequuntur totam fugit dolorum facilis perspiciatis tempore nam autem obcaecati ratione neque enim? Qui?</p>
-            <a id='LinkedIn' className='mx-2' href='/'><i class="fa-brands fa-linkedin fa-xl"></i></a>
-            <a id='Github' className='mx-2' href='/'><i class="fa-brands fa-github fa-xl"></i></a>
-            <a id='email' className='mx-2' href='/'><i class="fa-regular fa-envelope fa-xl"></i></a>
-            <a id='Instagram' className='mx-2' href='/'><i class="fa-brands fa-instagram fa-xl"></i></a>
+            <a id='Github' className='mx-2 my-3' href='/'><i class="fa-brands fa-github fa-xl"></i></a>
+            <a id='LinkedIn' className='mx-2 my-3' href='/'><i class="fa-brands fa-linkedin fa-xl"></i></a>
+            <a id='Twitter' className='mx-2 my-3' href='/'><i class="fa-brands fa-square-twitter fa-xl"></i></a>
+            <a id='Instagram' className='mx-2 my-3' href='/'><i class="fa-brands fa-instagram fa-xl"></i></a>
+            <a id='email' className='mx-2 my-3' href='/'><i class="fa-regular fa-envelope fa-xl"></i></a>
             </Col>
-            <Col xs={12} md={6} xl={5} style={{textAlign: "center"}}>
+            <Col xs={12} md={6} xl={5}  className="image-cropper" style={{textAlign: "center"}}>
             <img className="floating" src={image} alt="my_picture" />
             </Col>
         </Row>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,13 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Navbarr = () => {
   return (
     <>
     <Navbar expand="md" className="navbar">
       <Container>
-        <Navbar.Brand href="#home" className="navbar-brand">Home</Navbar.Brand>
+        <Navbar.Brand href="/" className="navbar-brand">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
