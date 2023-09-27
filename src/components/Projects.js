df2dd70f62ed96f4ba584da5ce350be0029ce721
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-28 02:05:19 +0530

    some changes in about-timeline, made projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Projects = () => {
+  return (
+    <div>
+      <Container>
+        <Row>
+            <Col>
+                <h1 id="project-headline">Projects</h1>
+            </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Projects
