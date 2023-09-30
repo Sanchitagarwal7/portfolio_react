aa282f3feb99f055a92dde28ca7d50bea42d9daa
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-30 12:49:38 +0530

    remopved mx-1

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,20 +11,20 @@ const Skills = () => {
             </Col>
         </Row>
         <Row>
-            <Col><span className="skill-name mx-1 my-5">html</span></Col>
-            <Col><span className="skill-name mx-1 my-5">ejs</span></Col>
-            <Col><span className="skill-name mx-1 my-5">css</span></Col>
-            <Col><span className="skill-name mx-1 my-5">bootstrap</span></Col>
-            <Col><span className="skill-name mx-1 my-5">javascript</span></Col>
-            <Col><span className="skill-name mx-1 my-5">jquery</span></Col>    
+            <Col><span className="skill-name my-5">html</span></Col>
+            <Col><span className="skill-name my-5">ejs</span></Col>
+            <Col><span className="skill-name my-5">css</span></Col>
+            <Col><span className="skill-name my-5">bootstrap</span></Col>
+            <Col><span className="skill-name my-5">javascript</span></Col>
+            <Col><span className="skill-name my-5">jquery</span></Col>    
         </Row>
         <Row>
-            <Col><span className="skill-name mx-1 my-5">mongoose</span></Col>
-            <Col><span className="skill-name skill-highlight mx-1 my-5">mongodb</span></Col>
-            <Col><span className="skill-name skill-highlight mx-1 my-5">express.js</span></Col>
-            <Col><span className="skill-name skill-highlight mx-1 my-5">react</span></Col>
-            <Col><span className="skill-name skill-highlight mx-1 my-5">node.js</span>   </Col>
-            <Col><span className="skill-name mx-1 my-5">c/c++</span></Col>
+            <Col><span className="skill-name my-5">mongoose</span></Col>
+            <Col><span className="skill-name skill-highlight my-5">mongodb</span></Col>
+            <Col><span className="skill-name skill-highlight my-5">express.js</span></Col>
+            <Col><span className="skill-name skill-highlight my-5">react</span></Col>
+            <Col><span className="skill-name skill-highlight my-5">node.js</span>   </Col>
+            <Col><span className="skill-name my-5">c/c++</span></Col>
         </Row>
     </Container>
     </>
