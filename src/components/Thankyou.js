7393be7ed6a6f65f32404a80d9e0ea47709404e3
Sanchit Agarwal <sanchita.g893@gmail.com>
2024-05-29 23:44:56 +0530

    resume setup for download

--- src/components/Thankyou.js
+++ src/components/Thankyou.js
@@ -8,7 +8,7 @@ const Thankyou = () => {
     <Container>
         <Row style={{marginTop: "20vh"}}>
             <Col id="resume">
-                <a href="#" class="neon-button">Download Resumé</a>
+                <a href="src/components/resume/Sanchit_Resume2.pdf" class="neon-button" download>Download Resumé</a>
                 <h5 id='sanchit'>Made by Sanchit 
                 <span className='heart mx-3'>
                 <i class="fa-regular fa-heart fa-sm fa-beat-fade" style={{"--fa-animation-duration": "0.5s"}}></i>
