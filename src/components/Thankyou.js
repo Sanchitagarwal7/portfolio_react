a6b5b39e09e64040afbebe04427c6c5b2519e7fd
Sanchit Agarwal <sanchita.g893@gmail.com>
2024-05-30 00:04:14 +0530

    resume download should work now

--- src/components/Thankyou.js
+++ src/components/Thankyou.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import ScrollAnimation from 'react-animate-on-scroll'
+import resume from './images/Sanchit_Resume.pdf'
 
 const Thankyou = () => {
   return (
@@ -8,7 +8,7 @@ const Thankyou = () => {
     <Container>
         <Row style={{marginTop: "20vh"}}>
             <Col id="resume">
-                <a class="neon-button" href="Sanchit_Resume.pdf" download="Sanchit_Resume">Download Resumé</a>
+                <a class="neon-button" href={resume} download="Sanchit_Resume">Download Resumé</a>
                 <h5 id='sanchit'>Made by Sanchit 
                 <span className='heart mx-3'>
                 <i class="fa-regular fa-heart fa-sm fa-beat-fade" style={{"--fa-animation-duration": "0.5s"}}></i>
